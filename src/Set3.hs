ecf6720ca752760cad17670dd06a3f08e00478af
Evgeny Gandzuk <e.gandzuk@intetics.com>
2018-12-29 12:18:15 +0300

    Generalizing pairs and cards

--- src/Set3.hs
+++ src/Set3.hs
@@ -9,3 +9,23 @@ allPairs :: [a] -> [b] -> [(a, b)]
 allPairs xas = concat . foldr (\b acc -> map (flip (,) b) xas : acc) []  
 -- I could have done this using Applicative
 -- allPairs xas xbs = (,) <$> xas <*> xbs 
+
+-- Poker hands
+data Card = Card (Int, String)
+
+instance Show Card where
+  show (Card(i, s)) = show i ++ s
+
+allCards :: [Int] -> [String] -> [Card]
+allCards is = concat . foldr (\b acc -> map (Card . flip (,) b) is : acc) []
+
+-- Generalizing pairs and cards
+allCombs :: (a -> b -> c) -> [a] -> [b] -> [c]
+allCombs f xas xbs = concat . foldr (\a acc -> map (f a) xbs : acc) [] $ xas
+
+allPairs' :: [a] -> [b] -> [(a, b)]
+allPairs' = allCombs (,)
+
+allCards' :: [Int] -> [String] -> [Card]
+allCards' = (map Card .) . allPairs
+
